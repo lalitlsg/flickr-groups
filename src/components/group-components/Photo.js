086a86ac3aee3e94f26e7777f4a273afa4c260e2
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-29 11:55:53 +0530

    image owner added

--- src/components/group-components/Photo.js
+++ src/components/group-components/Photo.js
@@ -1,5 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import { BsFillPersonFill } from "react-icons/bs";
+
+const Image = styled.img`
+  width: 100%;
+  border-radius: 5px;
+`;
+
+const Owner = styled.div`
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  opacity: 0;
+  border-radius: 5px;
+  padding: 5px;
+  background-color: white;
+`;
 
 const PhotoWrapper = styled.div`
   width: 100%;
@@ -9,17 +25,21 @@ const PhotoWrapper = styled.div`
   background-color: #fff;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.2);
   break-inside: avoid;
-`;
-const Image = styled.img`
-  width: 100%;
-  border-radius: 5px;
+  position: relative;
+  &:hover ${Owner} {
+    opacity: 1;
+  }
 `;
 
 const Photo = props => {
-  let { url } = props.photo;
+  console.log(props);
+  let { url, ownername } = props.photo;
   return (
     <PhotoWrapper>
       <Image src={url} alt="" />
+      <Owner>
+        <BsFillPersonFill /> {ownername}
+      </Owner>
     </PhotoWrapper>
   );
 };
